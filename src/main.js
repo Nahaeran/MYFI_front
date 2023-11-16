0d54026b47e1282ee954190dfd6422dcf3fdd44a
nahaeran <nh2545@naver.com>
2023-11-16 16:07:14 +0900

    init: initial commit 및 favicon 설정

---
+++ src/main.js
@@ -0,0 +1,30 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+import { ko } from 'vuetify/locale'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  locale: {
+    locale: 'ko',
+    messages: { ko }
+  }
+})
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(vuetify)
+
+app.mount('#app')
