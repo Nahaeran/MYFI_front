be36707ff3b37bef277ad79a6a19f42d15e7a044
nahaeran <nh2545@naver.com>
2023-11-20 17:55:44 +0900

    feat: exchange 구현 중

---
+++ src/components/ExchangeCalculater.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <v-card>
+    <v-form>
+      
+      <v-text-field
+        type="number"
+        label="한국 원"
+        prepend-icon="mdi-currency-krw"
+      ></v-text-field>
+    </v-form>    
+  </v-card>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/ExchangeView.vue
+++ src/views/ExchangeView.vue
@@ -1,13 +1,18 @@
 <script setup>
-
+import ExchangeCalculater from '@/components/ExchangeCalculater.vue'
 </script>
 
 <template>
-  <div>
-    <h1>exchange</h1>
+  <div class="container">
+    <h1><span class="color">환율</span> 계산기</h1>
+    <v-divider class="my-3"></v-divider>
+    <ExchangeCalculater />
   </div>
 </template>
 
 <style scoped>
-
+.container {
+  width: 1000px;
+  margin: 2rem auto;
+}
 </style>
